b1a72a2de6f69d1a59bb4bfcecee8deb797264cb
Nikolay Georgiev <azazen09@gmail.com>
2012-07-20 00:25:54 +0300

    added translation module

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
         	new FOS\UserBundle\FOSUserBundle(),
         	new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         	new Knp\Bundle\MenuBundle\KnpMenuBundle(),
