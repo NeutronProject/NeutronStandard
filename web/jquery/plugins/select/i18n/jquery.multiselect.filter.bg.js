1f22b184711bf57f4c26ded761dbdcc013c6e595
Nikolay Georgiev <azazen09@gmail.com>
2012-07-23 22:22:12 +0300

    multiselect resources

---
+++ web/jquery/plugins/select/i18n/jquery.multiselect.filter.bg.js
@@ -0,0 +1,10 @@
+/* Bulgarian initialisation for the jQuery UI multiselect plugin. */
+
+(function ( $ ) {
+
+$.extend($.ech.multiselectfilter.prototype.options, {
+	label: "Filter:",
+	placeholder: "Enter keywords"
+});
+
+})( jQuery );
