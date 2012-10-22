b4c08ffc4147619fe7b7a86909a69336f09b5be9
Nikolay Georgiev <azazen09@gmail.com>
2012-10-22 23:11:37 +0300

    added contact info

---
+++ src/AppBundle/Entity/ContactInfo.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of AppBundle
+*
+* (c) Zender <azazen09@gmail.com>
+*
+* This source file is subject to the MIT license that is bundled
+* with this source code in the file LICENSE.
+*/
+namespace AppBundle\Entity;
+
+use Neutron\Plugin\ContactBundle\Entity\AbstractContactInfo;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="contact_info")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\ContactInfoRepository")
+ * 
+ */
+class ContactInfo extends AbstractContactInfo
+{}
---
+++ src/AppBundle/Entity/Repository/ContactInfoRepository.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of AppBundle
+ *
+ * (c) Zender <azazen09@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace AppBundle\Entity\Repository;
+
+use Neutron\Plugin\ContactBundle\Entity\Repository\ContactInfoRepository as BaseRepository;
+
+class ContactInfoRepository extends BaseRepository
+{
+
+}
